f9939d1269dca55ecfdab843d93ae1ded809f6ed
Noel2002 <berwanoel@gmail.com>
2022-05-21 00:14:39 +0800

    setting up routes and creating dashboard route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Dashbooard from './routes/DashBoard/Dashbooard';
 import Home from './routes/Home'
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="nowoWeb" element={<Home />} />
+        <Route exact path="nowoWeb/dashboard" element={<Dashbooard />} />
+      </Routes>      
+    </Router>
   )
 }
 
