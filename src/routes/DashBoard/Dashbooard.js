a57d250ad0894e4a0ff8f3132f8bf7471970dcc3
Noel2002 <berwanoel@gmail.com>
2022-07-11 21:51:47 +0800

    creating main layout for dashboard

--- src/routes/DashBoard/Dashbooard.js
+++ src/routes/DashBoard/Dashbooard.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import styles from "./dashboard.module.css"
 
 const Dashbooard = () => {
   return (
-    <div>Dashbooard</div>
+    <div className={styles['root']}>
+      <div className={styles['aside']}>
+        aside
+      </div>
+      <div className={styles['main']}>
+        main
+      </div>
+    </div>
   )
 }
 
